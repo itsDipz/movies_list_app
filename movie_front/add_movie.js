67007a8e59aae453c882d61d7a44da53d5dda041
itsDipz <131451520+itsDipz@users.noreply.github.com>
2023-09-26 10:10:35 +0200

    uppdate

--- movie_front/add_movie.js
+++ movie_front/add_movie.js
@@ -14,3 +14,65 @@ export async function add_movie(a_title,a_user_id, a_password){
     let resource = response.json();
     console.log(resource);
 }
+
+export async function add_to_list(user_id, password, series_id) {
+    let body_for_request = {
+        action: "add_series_to_list",
+        user_id: user_id,
+        password: password,
+        series_id: series_id,
+    }
+    let patch_config = {
+        method: "PATCH",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify(body_for_request),
+    }
+    let response = fetch("https://teaching.maumt.se/apis/SR/v1/", patch_config);
+    let resource = response.json();
+    console.log(resource);
+}
+
+export async function remove_from_list(user_id, password, series_id) {
+    let body_for_request = {
+        action: "remove_series_to_list",
+        user_id: user_id,
+        password: password,
+        series_id: series_id,
+    }
+    let patch_config = {
+        method: "DELETE",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify(body_for_request),
+    }
+    let response = fetch("https://teaching.maumt.se/apis/SR/v1/", patch_config);
+    let resource = response.json();
+    console.log(resource);
+}
+
+export async function show_list(user_id, password) {
+  
+    let response = await fetch(`https://teaching.maumt.se/apis/SR/v1/?list&user_id=${user_id}&password=${password}`);
+    let resource = await response.json();
+    console.log(resource);
+
+    
+    let all_movies_container = document.querySelector(".all_the_movies");
+    let all_movies_elements = all_movies_container.querySelectorAll("div")
+    console.log(all_movies_elements);
+    let array_of_your_list_ele = [];
+    for (let index = 0; index < all_movies_elements.length; index++) {
+      resource.forEach(element => {
+        console.log(parseInt(all_movies_elements[index].dataset.id))
+            if(element === parseInt(all_movies_elements[index].dataset.id)){
+                array_of_your_list_ele.push(all_movies_elements[index]);
+            }
+       });
+    }
+    console.log(array_of_your_list_ele);
+
+    for (let i = 0; i < array_of_your_list_ele.length; i++) {
+        let element = array_of_your_list_ele[i];
+        document.querySelector(".my_ranking_list").appendChild(element);
+    }
+}
+
--- movie_front/init_app.js
+++ movie_front/init_app.js
@@ -1,4 +1,4 @@
-import { add_movie } from "./add_movie.js";
+import { add_movie, add_to_list, remove_from_list, show_list } from "./add_movie.js";
 
 export function init_app(user_id, password){
 
@@ -7,9 +7,7 @@ export function init_app(user_id, password){
         let resource = await response.json();
     }
 
-    async function take_away_movie_element(title){
-
-    }
+    
 
     async function create_all_movie_elements(){
         let response = await fetch("https://teaching.maumt.se/apis/SR/v1/?series"); 
@@ -18,29 +16,36 @@ export function init_app(user_id, password){
         all_movies.forEach(element => {
             let movie_element = document.createElement("div");
             movie_element.classList.add("drag_ele");
+            movie_element.dataset.id = element.series_id;
             movie_element.innerHTML = element.title;
             movie_element.setAttribute("draggable", true);
             document.querySelector(".all_the_movies").appendChild(movie_element);
         });
-
-        document.querySelector(".my_ranking_list").addEventListener("dragover", (event) => {
-            document.querySelectorAll(".drag_ele").forEach((movie_element) =>{
+        show_list(user_id, password);
+        let my_list = document.querySelector(".my_ranking_list");
+        my_list.addEventListener("dragover", (event) => {
+            the_movies.querySelectorAll(".drag_ele").forEach((movie_element) =>{
                 movie_element.addEventListener("dragend", (event) => {
                     document.querySelector(".my_ranking_list").appendChild(movie_element);
+                    add_to_list(user_id, password, parseInt(movie_element.dataset.id));
+                    
                 })
             })
         })
-
-        document.querySelector(".all_the_movies").addEventListener("dragover", (event) => {
-            document.querySelectorAll(".drag_ele").forEach((movie_element) =>{
+        let the_movies = document.querySelector(".all_the_movies")
+        the_movies.addEventListener("dragover", (event) => {
+            my_list.querySelectorAll(".drag_ele").forEach((movie_element) =>{
                 movie_element.addEventListener("dragend", (event) => {
                     document.querySelector(".all_the_movies").appendChild(movie_element);
+                    remove_from_list(user_id, password, parseInt(movie_element.dataset.id));
                 })
             })
         })
-        
+       
     }
     
+    
+
     console.log(user_id);
     console.log(password);
     document.querySelector("link").href = "./movie_front/movie_front.css"
@@ -70,10 +75,5 @@ export function init_app(user_id, password){
         </div>
     `;
     create_all_movie_elements();
-    document.querySelector(".add_movies_yourself").addEventListener(("click"), async () => {
-        console.log("add your own movie");
-        await add_movie("Come fly with me", user_id, password);
-        document.querySelector(".all_the_movies").innerHTML = "";
-        create_all_movie_elements();
-    });
+    // remove_from_list(user_id, password, 1);
 }
